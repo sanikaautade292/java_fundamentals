da789939405d75dc40a3d2bc4fe867c7637a885b
sanikaautade292 <sanikaautade2@gmail.com>
2026-02-12 22:58:25 +0530

    Add files via upload

---
+++ pattern/no1.java
@@ -0,0 +1,18 @@
+class no{
+	public static void main(String [] args){
+		for(int i=1 ; i<=10 ; i++){
+			if(i==7){
+				System.out.print(i);
+			}
+			elseif(i==8){
+				System.out.print(i);
+			}
+			elseif(i==9){
+				System.out.print(i);
+			}
+			else{
+				System.out.print("not found");
+			}
+		}
+	}
+}
---
+++ pattern/right4.java
@@ -0,0 +1,15 @@
+class Rtrangle4{
+	public static void main(String [] args){
+		for(int i=4 ; i>=1 ; i--){
+			for(int j=1 ; j<=4 ; j++){
+				if( i>=j){
+					System.out.print("* ");
+				}
+				else{
+					System.out.print("  ");
+  				}
+			}
+			System.out.println();
+		}
+	}
+}
