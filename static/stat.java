9307786e7f476b7c5ab9060aa9d88a8fb2d5f8a0
sanikaautade292 <sanikaautade2@gmail.com>
2026-02-12 23:00:18 +0530

    Add files via upload

---
+++ static/stat.java
@@ -0,0 +1,16 @@
+class static1{
+	public static void main(Strings... sanika){
+		static1.fun();
+		static1 s = new static1();
+		s.run();
+
+
+
+	}
+	static void fun(){
+		system.out.println("in fun");
+	}
+	void run(){
+		System.out.println("fan started");
+	}
+}
